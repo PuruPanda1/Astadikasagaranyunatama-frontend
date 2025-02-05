e066fe82b3c5f54cc05240ff8d37a4a533857d4f
PuruPanda <purabmodi2002@gmail.com>
2025-02-05 15:39:39 +0530

    Added Ship marker and changed API to test data

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import BlockLoader from './BlockLoader';
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+import BlockLoader from "./BlockLoader";
 
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-import axios from 'axios';
-
+import axios from "axios";
 
 const MapView = () => {
   const mapContainerRef = useRef();
@@ -16,115 +15,135 @@ const MapView = () => {
   const [points, setPoints] = useState([]);
   // get the path points from the Django API
   useEffect(() => {
-
     setLoading(true);
     const getMinPath = async () => {
       try {
-        const response = await axios.post('http://127.0.0.1:8000/api/get-points/', {
-          points: state.points,
-          weights: state.weights
-        }, {
-          headers: {
-            'Content-Type': 'application/json'
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/get-points-x/",
+          {
+            points: state.points,
+            weights: state.weights,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
-        });
+        );
 
         const data = response.data.path;
 
         setPoints(data);
 
-        console.log('Received Points:', data);
-
+        console.log("Received Points:", data);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
       }
-
     };
 
     getMinPath();
     setLoading(false);
-
-
   }, []);
 
   useEffect(() => {
     console.log(points.length === 0, isLoading);
     if (points.length === 0 || isLoading) return;
 
-
-    mapboxgl.accessToken = import.meta.env.VITE_REACT_APP_MAPBOX_ACCESS_TOKEN
+    mapboxgl.accessToken = import.meta.env.VITE_REACT_APP_MAPBOX_ACCESS_TOKEN;
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/satellite-v9',
+      style: "mapbox://styles/mapbox/satellite-v9",
       center: [points[0].longitude, points[0].latitude],
-      zoom: 12
+      zoom: 12,
     });
 
+    const startMarker = new mapboxgl.Marker({
+      element: document.createElement('div'),
+      anchor: 'center'
+    })
+      .setLngLat([
+        points[0].longitude,
+        points[0].latitude,
+      ])
+      .addTo(mapRef.current);
+
+    // Add ship icon to the marker element
+    startMarker.getElement().innerHTML = `
+      <img src="src/assets/icons/ship.png" style="width: 60px; height: 60px;" alt="Ship"/>
+    `;
+
+    const endMarker = new mapboxgl.Marker()
+      .setLngLat([
+        points[points.length - 1].longitude,
+        points[points.length - 1].latitude,
+      ])
+      .addTo(mapRef.current);
 
     // Creating the pathway geojson object
     const pathway = {
-      type: 'FeatureCollection',
+      type: "FeatureCollection",
       features: [
         {
-          type: 'Feature',
+          type: "Feature",
           properties: {},
           geometry: {
-            coordinates: points.map(point => [point.longitude, point.latitude]),
-            type: 'LineString'
-          }
-        }
-      ]
+            coordinates: points.map((point) => [
+              point.longitude,
+              point.latitude,
+            ]),
+            type: "LineString",
+          },
+        },
+      ],
     };
 
-    mapRef.current.on('load', () => {
-
+    mapRef.current.on("load", () => {
       // add the marker for each point to show variables about a point
 
-      mapRef.current.addSource('places', {
-        type: 'geojson',
+      mapRef.current.addSource("places", {
+        type: "geojson",
         data: {
-          type: 'FeatureCollection',
-          features: points.map(point => ({
-            type: 'Feature',
+          type: "FeatureCollection",
+          features: points.map((point) => ({
+            type: "Feature",
             properties: {
               // Change the description with points info --> wind_speed, weather_description, visibility, sea_level, distance, tide_height
               description: `<p style="color: black;">Wind Speed: ${point.location_parameters.wind_speed} m/s</p>
             <p style="color: black;">Weather: ${point.location_parameters.weather_description}</p>
             <p style="color: black;">Visibility: ${point.location_parameters.visibility} m</p>
             <p style="color: black;">Sea Level: ${point.location_parameters.sea_level} hPa</p>
-            <p style="color: black;">Distance: ${point.location_parameters.distance} km</p>`
-
+            <p style="color: black;">Distance: ${point.location_parameters.distance} km</p>`,
             },
             geometry: {
-              type: 'Point',
-              coordinates: [point.longitude, point.latitude]
-            }
-          }))
-        }
+              type: "Point",
+              coordinates: [point.longitude, point.latitude],
+            },
+          })),
+        },
       });
 
       mapRef.current.addLayer({
-        id: 'places',
-        type: 'circle',
-        source: 'places',
+        id: "places",
+        type: "circle",
+        source: "places",
         paint: {
-          'circle-color': '#4264fb',
-          'circle-radius': 6,
-          'circle-stroke-width': 2,
-          'circle-stroke-color': '#4264fb'
-        }
+          "circle-color": "#4264fb",
+          "circle-radius": 6,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#4264fb",
+        },
       });
 
       const popup = new mapboxgl.Popup({
         closeButton: false,
-        closeOnClick: false
+        closeOnClick: false,
       });
 
-      mapRef.current.on('mouseenter', 'places', (e) => {
-        mapRef.current.getCanvas().style.cursor = 'pointer';
+      mapRef.current.on("mouseenter", "places", (e) => {
+        mapRef.current.getCanvas().style.cursor = "pointer";
 
         const coordinates = e.features[0].geometry.coordinates.slice();
         const description = e.features[0].properties.description;
@@ -136,38 +155,37 @@ const MapView = () => {
         popup.setLngLat(coordinates).setHTML(description).addTo(mapRef.current);
       });
 
-      mapRef.current.on('mouseleave', 'places', () => {
-        mapRef.current.getCanvas().style.cursor = '';
+      mapRef.current.on("mouseleave", "places", () => {
+        mapRef.current.getCanvas().style.cursor = "";
         popup.remove();
       });
 
-
       // adding the pathway on the map
-      mapRef.current.addSource('pathway', {
-        type: 'geojson',
-        data: pathway
+      mapRef.current.addSource("pathway", {
+        type: "geojson",
+        data: pathway,
       });
 
       mapRef.current.addLayer({
-        type: 'line',
-        source: 'pathway',
-        id: 'line-background',
+        type: "line",
+        source: "pathway",
+        id: "line-background",
         paint: {
-          'line-color': 'blue',
-          'line-width': 6,
-          'line-opacity': 0.4
-        }
+          "line-color": "blue",
+          "line-width": 6,
+          "line-opacity": 0.4,
+        },
       });
 
       mapRef.current.addLayer({
-        type: 'line',
-        source: 'pathway',
-        id: 'line-dashed',
+        type: "line",
+        source: "pathway",
+        id: "line-dashed",
         paint: {
-          'line-color': 'blue',
-          'line-width': 6,
-          'line-dasharray': [0, 4, 3]
-        }
+          "line-color": "blue",
+          "line-width": 6,
+          "line-dasharray": [0, 4, 3],
+        },
       });
 
       const dashArraySequence = [
@@ -184,7 +202,7 @@ const MapView = () => {
         [0, 2, 3, 2],
         [0, 2.5, 3, 1.5],
         [0, 3, 3, 1],
-        [0, 3.5, 3, 0.5]
+        [0, 3.5, 3, 0.5],
       ];
 
       let step = 0;
@@ -194,16 +212,15 @@ const MapView = () => {
 
         if (newStep !== step) {
           mapRef.current.setPaintProperty(
-            'line-dashed',
-            'line-dasharray',
+            "line-dashed",
+            "line-dasharray",
             dashArraySequence[step]
           );
           step = newStep;
         }
 
         requestAnimationFrame(animateDashArray);
-      }
-
+      };
 
       animateDashArray(0);
     });
@@ -215,8 +232,12 @@ const MapView = () => {
     };
   }, [points, isLoading]);
 
-  return <div ref={mapContainerRef} style={{ height: '100vh', width: '100vw' }}></div>;
-
+  return (
+    <div
+      ref={mapContainerRef}
+      style={{ height: "100vh", width: "100vw" }}
+    ></div>
+  );
 };
 
 export default MapView;
